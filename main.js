351f5e3a6dba9695f313c22f95bbe0274872edb7
Avneesh112 <131769034+Avneesh112@users.noreply.github.com>
2023-06-07 15:04:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+senorita_song="";
+tqg_song="";
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
+scoreleftWrist = 0;
+scorerightWrist = 0;
+song_senorita = "";
+song_tqg = "";
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
+
+function preload(){
+    senorita_song = loadSound("Senorita.mp3");
+    tqg_song = loadSound("TQG.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+
+    fill("#00ff00");
+    stroke("#ff0000");
+
+    song_senorita = senorita_song.isPlaying();
+    console.log(song_senorita);
+
+    song_tqg = tqg_song.isPlaying();
+    console.log(song_tqg);
+
+    if(scoreleftWrist > 0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+        tqg_song.stop();
+        if(song_senorita == false){
+            senorita_song.play();
+        }
+        else{
+            console.log("Song Name: Senorita");
+            document.getElementById("song_id").innerHTML = "Song Name: Senorita";
+        }
+    }
+
+    if(scorerightWrist > 0.2){
+        circle(rightWrist_x,rightWrist_y,20);
+        senorita_song.stop();
+        if(song_tqg == false){
+            tqg_song.play();
+        }
+        else{
+            console.log("Song Name: TQG");
+            document.getElementById("song_id").innerHTML = "Song Name: TQG";
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log(scoreleftWrist);
+
+        scorerightWrist = results[0].pose.keypoints[10].score;
+        console.log(scorerightWrist);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
+}
